de078b411287464aa951d36070d84cf175ef2256
npnorman <npn.not@gmail.com>
2023-12-31 18:38:27 -0500

    Add project files.

---
+++ Condition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hp_system {
+    public class Condition {
+        //to customize this class, make a child of it!
+
+        int hpModifier;
+
+        public Condition() { 
+            hpModifier = 1;
+        }
+
+        public Condition(int hpModifier) {
+            this.hpModifier = hpModifier;
+        }
+
+        public int getHpModifier() {
+            return hpModifier;
+        }
+
+        public void setHpModifier(int hpModifier) {
+            this.hpModifier = hpModifier;
+        }
+
+        public virtual int HPFormula() {
+            //this returns the amount of hp lost or gained by the player
+            //this is meant to be replaced in a child function
+            return hpModifier * -1;
+        }
+
+        public virtual bool isValid() {
+            //this is meant to be replaced in a child function
+            //this returns false if condition should be removed from the player
+            return true; //this condition will never be removed unless manually by the user
+        }
+    }
+}
---
+++ Health.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hp_system
+{
+    public class Health
+    {
+        int hp; //health points
+        ArrayList conditions = new ArrayList();
+
+        public Health() {
+            hp = 0;
+        }
+
+        public Health(int hp) {
+            this.hp = hp;
+        }
+
+        public void SetHp(int hp) {
+            this.hp = hp;
+        }
+
+        public int GetHp() {
+            return hp;
+        }
+
+        public ArrayList getConditions() {
+            //will return condtions
+            return conditions;
+        }
+
+        public void AddCondition(Condition condition) {
+            conditions[0] = condition;
+        }
+
+        public void addHP(int hp) {
+            this.hp += hp;
+        }
+    }
+}
